58f53a759047bb26ff7d91932732818e40fc7869
xiongsp <37035570+xiongsp@users.noreply.github.com>
2022-07-03 17:20:30 +0800

    Update check_log.py

--- check_log.py
+++ check_log.py
@@ -4,7 +4,6 @@ history_data = []
 history_time = []
 coding_method = 'utf-8'
 
-
 if os.path.exists('./log'):
     # 列出文件夹下文件
     files = os.listdir('./log')
@@ -18,7 +17,8 @@ if os.path.exists('./log'):
                 last_line = lines[-1]
                 f.seek(0)
                 for line in f:
-                    if len(history_data) == 0 or line[20:].decode(coding_method) != history_data[-1]:
+                    if len(history_data) == 0 or line[20:].decode(
+                            coding_method) != history_data[-1]:
                         history_data.append(line[20:].decode(coding_method))
                         history_time.append(line[:19].decode(coding_method))
                         # print(line.decode(coding_method))
@@ -26,8 +26,9 @@ if os.path.exists('./log'):
                     if last_line == line:
                         history_data.append(line[20:].decode(coding_method))
                         history_time.append(line[:19].decode(coding_method))
+            print("-" * 10 + "新的一天" + "-" * 10)
             for time, data in zip(history_time, history_data):
                 print(time, data)
-
+            history_data, history_time = [], []
             # print(history_data)
             # print(history_time)
